874feb2dfaa13c09255e3ca5f68bc48df819f181
Conner Swanson <calm.calamity2@gmail.com>
2021-11-09 15:50:38 -0600

    Menu and Recently Viewed Automation tests and POM updates

--- src/__tests__/AutomationRecentlyViewed.test.ts
+++ src/__tests__/AutomationRecentlyViewed.test.ts
@@ -7,9 +7,9 @@ const driver: WebDriver = new Builder()
 const page = new BestBuyPage(driver);
 
 // variables for tests
-const searchQuery: string = "fish";
+const searchQuery: string = "cat";
 
-describe("BestBuy Search Tests", () => {
+describe("BestBuy Recently Viewed Tests", () => {
     // Before tests open the page and close the popup
     beforeAll(async () => {
       await page.navigate();
@@ -19,9 +19,14 @@ describe("BestBuy Search Tests", () => {
       await driver.quit();
     });
 
-    // Test that runs a basic search
-    test("Simple Search Test", async () => {
-      await page.doSearch(searchQuery);
-      expect(await page.getResults()).toContain(searchQuery);
+    // Test that verifies the recently viewed functionality
+    test("Recently Viewed Test", async () => {
+      await page.doSearchFeelingLucky(searchQuery);
+      const sku: string = await page.getText(page.skuTitle);
+      await driver.sleep(2000);
+      await page.goHome();
+      await page.click(page.recentlyViewed);
+      await driver.sleep(1000);
+      expect(await page.getText(page.recentlyViewedList)).toContain(sku);
     }, 60000);
   });
